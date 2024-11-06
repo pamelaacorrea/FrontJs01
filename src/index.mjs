08cb6cb28d2e845603864ef0f873aee5154abf72
pamelaa <correaprodocimop@gmail.com>
2024-11-06 12:34:11 +0000

    esconder resultados

--- src/index.mjs
+++ src/index.mjs
@@ -9,21 +9,36 @@ const soma = document.querySelector(".soma");
 const subtracao = document.querySelector(".subtracao");
 const multiplicacao = document.querySelector(".multiplicacao");
 const divisao = document.querySelector(".divisao");
+const btnLimpar = document.querySelector("#btn-limpar");
 
 // CRIAR FUNÇÃO - refatorar ou melhorar
 function calcular() {
   const n1 = Number(numero1.value); //CONVERSÃO P/ NUMBER
   const n2 = Number(numero2.value); //CONVERSÃO P/ NUMBER
   soma.innerHTML = `A soma de ${n1} e ${n2} é: ${n1 + n2}`;
-  subtracao.innerHTML = `A subtracao soma de ${n1} e ${n2} é: ${n1 - n2}`;
+  subtracao.innerHTML = `A subtracao de ${n1} e ${n2} é: ${n1 - n2}`;
   multiplicacao.innerHTML = `A multiplicacao de ${n1} e ${n2} é: ${n1 * n2}`;
   divisao.innerHTML = `A divisao de ${n1} e ${n2} é: ${n1 / n2}`;
-
-  return res;
+  //return res;
 }
-calcular();
+
+function limparFormulario() {
+  const n1 = Number(numero1.value); //CONVERSÃO P/ NUMBER
+  const n2 = Number(numero2.value); //CONVERSÃO P/ NUMBER
+  if (n1 && n2) {
+    const form = document.querySelector("form");
+    form.reset(); // LIMPAR O FORMULÁRIO
+    document.querySelector("#numero1").focus(); // FOCA NO N1
+  }
+}
 
 //ADICIONAR ESCUTADOR DE EVENTOS
-btnCalcular.addEventListener("click", function () {
-  alert(calcular());
+btnCalcular.addEventListener("click", function (evento) {
+  evento.preventDefault(); // DEIXA DE ATUALIZAR...
+  calcular();
+});
+
+btnLimpar.addEventListener("click", function (evento) {
+  evento.preventDefault();
+  limparFormulario();
 });
